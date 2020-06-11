cd35dfc5a9d107ef0ef8d965c4b9329b4dbe2a62
PatiCosta <patricosouza@hotmail.com>
2020-06-11 11:54:14 -0300

    changes

---
+++ src/App/controllers/members.js
@@ -0,0 +1,84 @@
+const { age, date } = require('../../lib/utils')
+const Intl = require('intl')
+const Member = require('../models/Member')
+
+
+module.exports = {
+    index(req, res) {
+        let { filter, page, limit } = req.query
+
+        page = page || 1
+        limit = limit || 5
+
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback (members){
+                const pagination = {
+                    total: Math.ceil(members[0].total / limit),
+                    page
+                }
+
+                return res.render("members/index", {members, filter, pagination})
+            }
+        }
+
+        Member.paginate(params)
+
+    },
+    create(req, res) {
+
+        Member.instructorsSelectOptions(function(options){
+            return res.render('members/create', {instructorOptions: options})
+        })
+
+    },
+    show(req, res) {
+        Member.find(req.params.id, function(member) {
+            if (!member) return res.send("member not found")
+
+            member.birth = date(member.birth).birthDay
+
+            return res.render('members/show', {member})
+        })
+    },
+    post(req, res) {
+        Member.create(req.body, function(member){
+            return res.redirect(`/members/${member.id}`)
+        })
+    },
+    edit(req, res) {
+
+        Member.find(req.params.id, function(member) {
+            if (!member) return res.send("member not found")
+
+            member.birth = date(member.birth).iso
+
+            Member.instructorsSelectOptions(function(options){
+                return res.render('members/edit', {member, instructorOptions: options})
+            })
+        })
+    },
+    put(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields')
+            }
+        }
+
+        Member.update(req.body, function() {
+            return res.redirect(`/members/${req.body.id}`)
+        })
+    },
+    delete(req, res) {
+        Member.delete(req.body.id, function() {
+            return res.redirect(`/members`)
+        })
+    },
+}
---
+++ src/App/models/instructor.js
@@ -0,0 +1,137 @@
+const db = require('../../config/db')
+const { date } = require('../../lib/utils')
+
+module.exports = {
+    all(callback) {
+        db.query('SELECT instructors.*, count (members) AS total_students FROM instructors LEFT JOIN members ON (members.instructor_id = instructors.id) GROUP BY instructors.id ORDER BY total_students DESC', function(err, results){
+            if(err) throw `Database error! ${err}` 
+
+            let instructors = results.rows
+
+            for ( instructor of instructors) {
+                const services = instructor.services.toString().split(',')
+                instructor.services = services
+            }
+
+
+
+            return callback(instructors)
+        })
+    },
+    create(data, callback) {
+        const keys = Object.keys(data)
+
+        for (key of keys) {
+            if (data[key] == "") {
+                return res.send('Please, fill all fields')
+            }
+        }
+    
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
+        const values = [
+            data.name,
+            data.avatar_url,
+            data.gender,
+            data.services,
+            date(data.birth).iso,
+            date(Date.now()).iso
+        ]
+        
+        db.query(query, values, function(err, results) {
+            if(err) throw `Database error! ${err}` 
+
+            return callback(results.rows[0])
+        })
+    },
+    find(id, callback) {
+        db.query(`SELECT * FROM instructors WHERE id = $1`, [id], function(err, results){
+            if(err) throw `Database error! ${err}` 
+
+            return callback(results.rows[0])
+        })
+    },
+    findBy(filter, callback){
+        db.query(`SELECT instructors.*, count (members) AS total_students FROM instructors LEFT JOIN members ON (members.instructor_id = instructors.id) WHERE instructors.name ILIKE '%${filter}%' OR instructors.services ILIKE '%${filter}%' GROUP BY instructors.id ORDER BY total_students DESC`, function(err, results){
+            if(err) throw `Database error! ${err}` 
+
+            let instructors = results.rows
+
+            for ( instructor of instructors) {
+                const services = instructor.services.toString().split(',')
+                instructor.services = services
+            }
+
+            return callback(instructors)
+        })
+    },
+    update(data, callback) {
+        const query = `UPDATE instructors SET avatar_url=($1), name=($2), birth=($3), gender=($4), services=($5) WHERE id = $6`
+        const values = [
+            data.avatar_url,
+            data.name,
+            date(data.birth).iso,
+            data.gender,
+            data.services,
+            data.id
+        ]
+
+        db.query(query, values, function(err, results) {
+            if(err) throw `Database error! ${err}` 
+
+            return callback()
+        })
+    },
+    delete(id, callback) {
+        db.query(`DELETE FROM instructors WHERE id = $1`, [id], function(err, results) {
+            if(err) throw `Database error! ${err}` 
+
+            return callback()  
+        })
+    },
+    paginate(params){
+        const { filter, limit, offset, callback } = params
+
+        let query = "",
+            filterQuery = "",
+            totalQuery = `(SELECT count(*) FROM instructors) AS total`
+
+
+        if ( filter ) {
+
+            filterQuery = ` 
+            WHERE instructors.name ILIKE '%${filter}%'
+            OR instructors.services ILIKE '%${filter}%'`
+
+            totalQuery = `(SELECT count(*) FROM instructors ${filterQuery}) AS total`
+        }
+
+        query = `SELECT instructors.*, ${totalQuery}, count(members) AS total_students
+        FROM instructors
+        LEFT JOIN members ON (instructors.id = members.instructor_id) 
+        ${filterQuery}
+        GROUP BY instructors.id LIMIT $1 OFFSET $2`
+
+        db.query(query, [limit, offset], function(err, results){
+            if (err)  throw `Database Error! ${err}`
+
+            let instructors = results.rows
+
+            for ( instructor of instructors) {
+                const services = instructor.services.toString().split(',')
+                instructor.services = services
+            }
+
+            callback(instructors)
+        })
+    }
+}
